1ab33c7c6346f1dbd6c4606dca9d12d387a106b5
Leonard-McCook <leonard.mccookcarr@outlook.com>
2022-07-24 14:59:24 -0400

    Add and setup google font

--- pages/index.js
+++ pages/index.js
@@ -12,6 +12,11 @@ export default function Home() {
 
       <main className="py-5">
         <h1 className="text-xl md:text-5xl text-center font-bold py-10">Employee Management</h1>
+        <div className="container mx-auto flex justify-between py-5 border-b">
+          <div className="left flex gap-3">
+
+          </div>
+        </div>
       </main>
 
     </section>
