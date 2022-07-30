96ff297fdf7be3f024b8adcd41d3f91502ae0ef1
Leonard-McCook <leonard.mccookcarr@outlook.com>
2022-07-30 10:50:58 -0400

    Create database folder and add  mock json data

--- components/table.js
+++ components/table.js
@@ -1,4 +1,7 @@
 import { BiEdit, BiTrashAlt } from "react-icons/bi";
+import data from "../database/data.json";
+
+
 export default function table() {
   return (
     <table className="min-w-full table-auto">
@@ -25,30 +28,37 @@ export default function table() {
             </tr>
         </thead>
         <tbody className="bg-gray-200">
-            <tr className="bg-gray-50 text-center">
+            {
+                data.map((obj, i) => <Tr {...obj} key={i}/>)
+            }
+        </tbody>
+    </table>
+  )
+}
+
+function Tr({id, name, avatar, email, salary, date, status}){
+    return(
+        <tr className="bg-gray-50 text-center">
                 <td className="px-16 py-2 flex flex-row items-center">
-                    <img src="#" alt="" />
-                    <span className="text-center ml-2 font-semibold">Marcus Fenix</span>
+                    <img src={avatar || '#'} alt="Avatar picture" />
+                    <span className="text-center ml-2 font-semibold">{name || "Unknown"}</span>
                 </td>
                 <td className="px-16 py-2">
-                    <span>marcus.fenix@gmail.com</span>
+                    <span>{email || "Unknown"}</span>
                 </td>
                 <td className="px-16 py-2">
-                    <span>$85,000</span>
+                    <span>{salary || "Unknown"}</span>
                 </td>
                 <td className="px-16 py-2">
-                    <span>21 B.E.</span>
+                    <span>{date || "Unknown"}</span>
                 </td>
                 <td className="px-16 py-2">
-                    <button className="cursor"><span className="bg-green-500 text-white px-5 py-1 rounded-full">Active</span></button>
+                    <button className="cursor"><span className="bg-green-500 text-white px-5 py-1 rounded-full">{status || "Unknown"}</span></button>
                 </td>
                 <td className="px-16 py-2 flex justify-around gap-5 mt-2">
                     <button className="cursor"><BiEdit size={25} color={"rgb(34,197,94)"}></BiEdit></button>
                     <button className="cursor"><BiTrashAlt size={25} color={"rgb(244,63,94)"}></BiTrashAlt></button>
                 </td>
             </tr>
-
-        </tbody>
-    </table>
-  )
+    )
 }
