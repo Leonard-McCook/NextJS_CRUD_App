9b0bc12fdd18614c573361f4502c45e448f5c865
Leonard-McCook <leonard.mccookcarr@outlook.com>
2022-08-01 17:17:33 -0400

    Add model folder and create schema

--- database/controller.js
+++ database/controller.js
@@ -1,10 +1,13 @@
 // Controller
+import Users from '../model/user';
 
 export async function getUsers (req, res) {
     try {
-        res.status(200).json({ user: "GET Request" })
+        const users = await Users.find({})
+
+        if(!users) return res.status(404).json({ error: "Data not Found"})
+        res.status(200).json(users)
     } catch (error) {
         res.status(404).json({ error: "An Error Ocurred While Trying to Fetch The Data" })
     }
-
 }
