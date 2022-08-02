6028a9cb7f343f580eb690ee4f32d9cf11d32581
Leonard-McCook <leonard.mccookcarr@outlook.com>
2022-08-02 13:11:49 -0400

    Post data to mongodb

--- database/controller.js
+++ database/controller.js
@@ -1,6 +1,7 @@
 // Controller
 import Users from '../model/user';
 
+// get: https//localhost:3000/api/users
 export async function getUsers (req, res) {
     try {
         const users = await Users.find({})
@@ -9,5 +10,18 @@ export async function getUsers (req, res) {
         res.status(200).json(users)
     } catch (error) {
         res.status(404).json({ error: "An Error Ocurred While Trying to Fetch The Data" })
+    }
+}
+
+// post: https//localhost:3000/api/users
+export async function postUser (req, res) {
+    try {
+        const formData = req.body;
+        if(!formData) return res.status(404).json({ error: "Form Data Not Found"});
+        Users.create( formData, function(err, data){
+            return res.status(200).json(data)
+    })
+    } catch (error) {
+       return res.status(404).json({error})
     }
 }
